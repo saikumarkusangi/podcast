ee2d963baa38ddfeb45d59b54214ac6388aff445
Sai Kumar Kusangi <93697313+saikumarkusangi@users.noreply.github.com>
2023-05-14 17:00:14 +0530

    Delete server directory

--- server/api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import categoriesModel,podcastModel
-# Register your models here.
-admin.site.register(categoriesModel)
-admin.site.register(podcastModel)
